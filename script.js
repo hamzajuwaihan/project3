2261047851b9dc142f6ac6b94822a72c4fb1803a
Hamza Juwaihan <hamzajuwaihan@gmail.com>
2022-08-28 16:00:36 +0300

    combine all scripts in one file

---
+++ script.js
@@ -0,0 +1,491 @@
+document.getElementById("welcomeMessage").innerHTML = `Welcome ${sessionStorage.name}`
+document.getElementById("start").addEventListener("click", (e) => {
+    document.getElementById("fir").style.display = "flex";
+})
+
+
+const p1Text = document.getElementById("p1_text");
+const p2Text = document.getElementById("p2_text");
+const p3Text = document.getElementById("p3_text");
+const selection = document.getElementById("employeeNameSelect");
+
+
+
+function showOne() {
+    p1Text.style.display = 'block';
+}
+
+function showTwo() {
+    p2Text.style.display = 'block';
+}
+
+function showThree() {
+    p3Text.style.display = 'block';
+}
+
+
+
+selection.addEventListener('change', (event) => {
+
+    document.getElementById("q-0").textContent = `${event.target.value} personality and charisma is truly magnetic.`;
+    document.getElementById("q-1").textContent = `${event.target.value} is a person of charisma and influence.`;
+    document.getElementById("q-2").textContent = `${event.target.value} is a very soft spoken person and never lose his temper.`;
+    document.getElementById("q-3").textContent = `${event.target.value} dress and personal appearance.`;
+    document.getElementById("q-4").textContent = `${event.target.value} is accountable for her tasks.`;
+    document.getElementById("q-5").textContent = `${event.target.value} behavior according to the company’s ethical code.`;
+    document.getElementById("q-6").textContent = `${event.target.value}  the employee demonstrated the highest level of understanding of
+      the most complex design and implementation issues at every stage
+      of development.`;
+    document.getElementById("q-7").textContent = `${event.target.value} has an exceptional way of finding things and get them
+      improved.`;
+    document.getElementById("q-8").textContent = `${event.target.value} understands complicated issues, even when outside of
+      his direct area of expertise.`;
+
+});
+
+
+let mySelect = document.getElementById('employeeNameSelect');
+mySelect.onchange = () => {
+    storage(mySelect.value);
+}
+function storage(empName) {
+    let emp1Arr = [];
+    // Q1 (Personal Critieria);
+
+    let pfe = document.getElementById('p-f-e');
+    let pfg = document.getElementById('p-f-g');
+    let pfp = document.getElementById('p-f-p');
+
+    pfe.onclick = () => {
+        emp1Arr[0] = `Excellent`;
+    }
+    pfg.onclick = () => {
+        emp1Arr[0] = `Good`
+
+    }
+    pfp.onclick = () => {
+        emp1Arr[0] = `Poor`
+    }
+
+    // Q2 (Personal Critieria);
+    let pse = document.getElementById('p-s-e');
+    let psg = document.getElementById('p-s-g');
+    let psp = document.getElementById('p-s-p');
+
+
+    pse.onclick = () => {
+        emp1Arr[1] = `Excellent`
+
+    }
+    psg.onclick = () => {
+        emp1Arr[1] = `Good`
+
+    }
+    psp.onclick = () => {
+        emp1Arr[1] = `Poor`
+
+    }
+
+    let pte = document.getElementById('p-t-e');
+    let ptg = document.getElementById('p-t-g');
+    let ptp = document.getElementById('p-t-p');
+
+    pte.onclick = () => {
+        emp1Arr[2] = `Excellent`
+
+
+    }
+    ptg.onclick = () => {
+        emp1Arr[2] = `Good`
+
+
+    }
+    ptp.onclick = () => {
+        emp1Arr[2] = `Poor`
+
+
+    }
+    // Q1 (Professional Critieria);
+
+    let profe = document.getElementById('pro-f-e');
+    let profg = document.getElementById('pro-f-g');
+    let profp = document.getElementById('pro-f-p');
+
+    profe.onclick = () => {
+        emp1Arr[3] = `Excellent`
+
+
+    }
+    profg.onclick = () => {
+        emp1Arr[3] = `Good`
+
+    }
+    profp.onclick = () => {
+        emp1Arr[3] = `Poor`
+
+    }
+    // Q2 (Professional Critieria);
+
+    let prose = document.getElementById('pro-s-e');
+    let prosg = document.getElementById('pro-s-g');
+    let prosp = document.getElementById('pro-s-p');
+
+    prose.onclick = () => {
+        emp1Arr[4] = `Excellent`
+
+    }
+    prosg.onclick = () => {
+        emp1Arr[4] = `Good`
+
+    }
+    prosp.onclick = () => {
+        emp1Arr[4] = `Poor`
+
+    }
+    // Q3 (Professional Critieria);
+
+    let prote = document.getElementById('pro-t-e');
+    let protg = document.getElementById('pro-t-g');
+    let protp = document.getElementById('pro-t-p');
+
+    prote.onclick = () => {
+        emp1Arr[5] = `Excellent`
+
+    }
+    protg.onclick = () => {
+        emp1Arr[5] = `Good`
+
+    }
+    protp.onclick = () => {
+        emp1Arr[5] = `Poor`
+
+    }
+
+    // Q1 (technicalSection Critieria);
+
+    let tfe = document.getElementById('t-f-e');
+    let tfg = document.getElementById('t-f-g');
+    let tfp = document.getElementById('t-f-p');
+
+    tfe.onclick = () => {
+        emp1Arr[6] = `Excellent`
+
+    }
+    tfg.onclick = () => {
+        emp1Arr[6] = `Good`
+
+
+    }
+    tfp.onclick = () => {
+        emp1Arr[6] = `Poor`
+
+    }
+
+    // Q2 (technicalSection Critieria);
+
+    let tse = document.getElementById('t-s-e');
+    let tsg = document.getElementById('t-s-g');
+    let tsp = document.getElementById('t-s-p');
+
+    tse.onclick = () => {
+        emp1Arr[7] = `Excellent`
+
+    }
+    tsg.onclick = () => {
+        emp1Arr[7] = `Good`
+
+    }
+    tsp.onclick = () => {
+        emp1Arr[7] = `Poor`
+
+    }
+
+    // Q3 (technicalSection Critieria);
+
+    let tte = document.getElementById('t-t-e');
+    let ttg = document.getElementById('t-t-g');
+    let ttp = document.getElementById('t-t-p');
+
+    tte.onclick = () => {
+        emp1Arr[8] = `Excellent`
+
+        localStorage.setItem(empName, JSON.stringify(emp1Arr));
+    }
+    ttg.onclick = () => {
+        emp1Arr[8] = `Good`
+
+        localStorage.setItem(empName, JSON.stringify(emp1Arr));
+    }
+    ttp.onclick = () => {
+        emp1Arr[8] = `Poor`
+
+        localStorage.setItem(empName, JSON.stringify(emp1Arr));
+    }
+}
+
+/* end of evaluation page script */
+
+
+/*start of report page */
+
+document.getElementById("start").addEventListener("click", (e) => {
+    document.getElementById("fir").style.display = "flex";
+    document.getElementById("recomendationSystem").style.display = "flex"
+
+})
+
+
+
+
+
+
+
+const selection2 = document.getElementById("employeeNameSelect2")
+selection2.addEventListener('change', (event) => {
+
+    document.getElementById("q-0").textContent = `${event.target.value} personality and charisma is truly magnetic.`;
+    document.getElementById("q-1").textContent = `${event.target.value} is a person of charisma and influence.`;
+    document.getElementById("q-2").textContent = `${event.target.value} is a very soft spoken person and never lose his temper.`;
+    document.getElementById("q-3").textContent = `${event.target.value} dress and personal appearance.`;
+    document.getElementById("q-4").textContent = `${event.target.value} is accountable for her tasks.`;
+    document.getElementById("q-5").textContent = `${event.target.value} behavior according to the company’s ethical code.`;
+    document.getElementById("q-6").textContent = `${event.target.value}  the employee demonstrated the highest level of understanding of
+        the most complex design and implementation issues at every stage
+        of development.`;
+    document.getElementById("q-7").textContent = `${event.target.value} has an exceptional way of finding things and get them
+        improved.`;
+    document.getElementById("q-8").textContent = `${event.target.value} understands complicated issues, even when outside of
+        his direct area of expertise.`;
+    myAnswers = JSON.parse(localStorage.getItem(myEmp));
+
+});
+let r0 = document.getElementById('r0')
+let r1 = document.getElementById('r1')
+let r2 = document.getElementById('r2')
+let r3 = document.getElementById('r3')
+let r4 = document.getElementById('r4')
+let r5 = document.getElementById('r5')
+let r6 = document.getElementById('r6')
+let r7 = document.getElementById('r7')
+let r8 = document.getElementById('r8')
+
+let l0 = document.getElementById('l0')
+let l1 = document.getElementById('l1')
+let l2 = document.getElementById('l2')
+let l3 = document.getElementById('l3')
+let l4 = document.getElementById('l4')
+let l5 = document.getElementById('l5')
+let l6 = document.getElementById('l6')
+let l7 = document.getElementById('l7')
+let l8 = document.getElementById('l8')
+
+
+let reco = document.getElementById("recommendArea")
+
+
+let re = 0;
+
+let mySelect2 = document.getElementById('employeeNameSelect2')
+let myEmp;
+let myAnswers;
+mySelect2.onchange = () => {
+    myEmp = mySelect2.value;
+    myAnswers = JSON.parse(localStorage.getItem(myEmp));
+    let recommendations = '';
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(`l${i}`).textContent = myAnswers[i];
+        console.log(document.getElementById(`l${i}`).textContent)
+        if (document.getElementById(`l${i}`).textContent === 'Excellent') {
+            document.getElementById(`r${i}`).style.borderColor = 'green'
+        } else if (document.getElementById(`l${i}`).textContent === 'Good') {
+            document.getElementById(`r${i}`).style.borderColor = 'yellow'
+        } else {
+            document.getElementById(`r${i}`).style.borderColor = 'red'
+        }
+    }
+
+    if ((l0.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>more charisma';
+    }
+    if ((l1.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>more influence';
+    }
+    if ((l2.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>control temper';
+    }
+    if ((l3.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>work on appearance';
+    }
+    if ((l4.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>must be accountable';
+    }
+    if ((l5.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>must respect ethical code';
+    }
+    if ((l6.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>more understanding of the most complex design';
+    }
+    if ((l7.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>must be creative';
+    }
+    if ((l8.textContent === 'Poor')) {
+        re = 1;
+        recommendations += '<br>must think out of the box';
+    }
+
+
+    if (re === 1) {
+        reco.innerHTML = `we advice you with the following based on your evaluation so you keep your company as its best!!${recommendations}`
+
+    } else {
+        reco.innerHTML = `not need Recommendations!!`
+    }
+}
+/* end of report */
+/* start of register script */
+const form = document.getElementById("sign-up");
+const emailField = document.getElementById("email");
+const phoneField = document.getElementById("phone");
+const passField = document.getElementById("pass");
+const nameField = document.getElementById("name");
+
+form.addEventListener("submit", (e) => {
+
+    if (nameField.value == null) {
+        document.getElementById("name-error").textContent =
+            "The name is required.";
+        e.preventDefault();
+    }
+    if (emailField.value == null || emailField.value == "") {
+        document.getElementById("email-error").textContent =
+            "The email field is required.";
+    } else if (
+        !emailField.value.match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+        document.getElementById("email-error").textContent =
+            "The email must be a valid email address.";
+        e.preventDefault();
+    } else {
+        document.getElementById("email-error").textContent = "";
+    }
+    if (phoneField.value == "" || phoneField.value == null) {
+        document.getElementById("tel-error").textContent =
+            "The Mobile field is required.";
+        e.preventDefault();
+    } else if (phoneField.value.length < 10) {
+        document.getElementById("tel-error").textContent =
+            "The mobile must be 10 characters.";
+        e.preventDefault();
+    } else {
+        document.getElementById("tel-error").textContent = "";
+    }
+
+    if (passField.value == "" || passField.value == null) {
+        document.getElementById("pass-error").textContent =
+            "The password field is required.";
+        e.preventDefault();
+    } else if (passField.value.length < 6) {
+        document.getElementById("pass-error").textContent =
+            "The password must be at least 6 characters.";
+        e.preventDefault();
+    } else if (passField.value.length > 18) {
+        document.getElementById("pass-error").textContent =
+            "The password may not be greater than 18 characters.";
+        e.preventDefault();
+    } else {
+        document.getElementById("pass-error").textContent = "";
+    }
+
+    let formData = JSON.parse(localStorage.getItem("formData")) || [];
+    formData.push({
+        name: nameField.value,
+        email: emailField.value,
+        password: passField.value,
+        phone: phoneField.value,
+    });
+    localStorage.setItem("formData", JSON.stringify(formData));
+    e.preventDefault();
+});
+/* end of register page*/
+
+/* start of login page */
+const signInForm = document.getElementById("log-in");
+const loginemailField = document.getElementById("login-email");
+
+const loginpassField = document.getElementById("login-pass");
+signInForm.addEventListener("submit", (e) => {
+    if (loginemailField.value == null || loginemailField.value == "") {
+        document.getElementById("email-error").textContent =
+            "The email field is required.";
+    } else if (
+        !loginemailField.value.match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+        document.getElementById("email-error").textContent =
+            "The email must be a valid email address.";
+        e.preventDefault();
+    } else {
+        document.getElementById("email-error").textContent = "";
+    }
+
+    if (loginpassField.value == "" || loginpassField.value == null) {
+        document.getElementById("pass-error").textContent =
+            "The password field is required.";
+        e.preventDefault();
+    } else if (loginpassField.value.length < 6) {
+        document.getElementById("pass-error").textContent =
+            "The password must be at least 6 characters.";
+        e.preventDefault();
+    } else if (loginpassField.value.length > 18) {
+        document.getElementById("pass-error").textContent =
+            "The password may not be greater than 18 characters.";
+        e.preventDefault();
+    } else {
+        document.getElementById("pass-error").textContent = "";
+    }
+    let found = false;
+    let userIndex = null;
+    let formData = JSON.parse(localStorage.getItem("formData")) || [];
+    formData.forEach((element, arrayIndex) => {
+        if (element.email == emailField.value) {
+            found = true;
+            userIndex = arrayIndex;
+        }
+    });
+
+    if (found && formData[userIndex].password == loginpassField.value) {
+        document.getElementById("message").innerHTML =
+            "logged in successfuly ";
+
+        sessionStorage.email = formData[userIndex].email;
+        sessionStorage.pass = formData[userIndex].password;
+        sessionStorage.name = formData[userIndex].name;
+
+        window.open('evaluation.html', '_blank');
+
+        e.preventDefault();
+    } else if (found && formData[userIndex].password !== loginpassField.value) {
+        document.getElementById("message").innerHTML = "Wrong password";
+        e.preventDefault();
+    } else if (!found) {
+        document.getElementById("message").innerHTML = "account not found";
+        e.preventDefault();
+    } else {
+        document.getElementById("message").innerHTML = "";
+    }
+    e.preventDefault();
+});
+
+/* end of login page*/
